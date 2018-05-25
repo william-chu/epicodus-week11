e6fd1557ace50ab9186e666a1916b98dcae8a365
William Chu <williamchu51@gmail.com>
2018-05-25 11:42:13 -0700

    establish menu modal function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
-// import { function } from './class';
 import './styles.css';
-import $ from 'jquery';
 
-$(document).ready(function() {
+// Get the modal
+var menuModal = document.getElementById('menu-modal');
 
-});
+// Get the button that opens the modal
+var menuBtn = document.getElementById("menu-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+menuBtn.onclick = function() {
+  menuModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  menuModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == menuModal) {
+    menuModal.style.display = "none";
+  }
+}
